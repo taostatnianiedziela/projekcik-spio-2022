a34306d2dccbb89390134fb2545bc20700ed4342
LukaszJal <67760453+LukaszJal@users.noreply.github.com>
2022-06-12 13:07:41 +0200

    fix , suma

---
+++ app/src/main/java/eu/jaloszynski/splitit/helpers/OnItemClickListenerFriends.java
@@ -0,0 +1,7 @@
+package eu.jaloszynski.splitit.helpers;
+
+import eu.jaloszynski.splitit.persistence.Friends;
+
+public interface OnItemClickListenerFriends {
+    void onItemClick(Friends item);
+}
